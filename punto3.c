6a45297d3767b6d59a49fabc5f96f205d05975d2
Rocio Hidalgo Bini <hidalgobinirocio@gmail.com>
2022-04-21 10:37:24 -0300

    .

--- punto3.c
+++ punto3.c
@@ -11,16 +11,16 @@ typedef struct Tarea{
 
 typedef struct Nodo{
     TAREA T;
-    NODO *Siguiente;
+    struct Nodo * Siguiente;
 }NODO;
 
 NODO *crearListaVacia();
 void CargarTareas(NODO *lista, int cantidad);
-void mostrarTarea(TAREA **tarea, int cantidad);
-void moverTareas(TAREA **pTareas, TAREA **pTareasHechas, int cantidad);
-void mostrarTareasHechasYPendientes(TAREA **pTareasHechas, TAREA **pTareasPendientes, int cantidad);
-TAREA *BuscarTareaPorID(TAREA **tarea, int cant);
-TAREA *BuscarTareaPorPalabra(TAREA **tarea, int cant);
+void mostrarTarea(NODO *lista, int cantidad);
+// void moverTareas(TAREA **pTareas, TAREA **pTareasHechas, int cantidad);
+// void mostrarTareasHechasYPendientes(TAREA **pTareasHechas, TAREA **pTareasPendientes, int cantidad);
+// TAREA *BuscarTareaPorID(TAREA **tarea, int cant);
+// TAREA *BuscarTareaPorPalabra(TAREA **tarea, int cant);
 
 int main(){
     srand(time(NULL));
@@ -35,29 +35,29 @@ int main(){
 
 
     CargarTareas(ListaPendientes, cantTareas);
-    mostrarTarea(pTareas, cantTareas);
-    moverTareas(pTareas, pTareasHechas, cantTareas);
+    mostrarTarea(ListaPendientes, cantTareas);
+    // moverTareas(pTareas, pTareasHechas, cantTareas);
 
-    mostrarTareasHechasYPendientes(pTareasHechas, pTareas, cantTareas);
+    // mostrarTareasHechasYPendientes(pTareasHechas, pTareas, cantTareas);
 
-    tarea = BuscarTareaPorID(pTareas, cantTareas);
-    if(tarea != NULL){
-        printf("\n------- TAREA ENCONTRADA -------\n");
-        printf("Tarea ID: %d\n", tarea->TareaID);
-        printf("Descripcion: %s\n", tarea->Descripcion);
-        printf("Duracion: %d\n", tarea->Duracion);
-    }
+    // tarea = BuscarTareaPorID(pTareas, cantTareas);
+    // if(tarea != NULL){
+    //     printf("\n------- TAREA ENCONTRADA -------\n");
+    //     printf("Tarea ID: %d\n", tarea->TareaID);
+    //     printf("Descripcion: %s\n", tarea->Descripcion);
+    //     printf("Duracion: %d\n", tarea->Duracion);
+    // }
 
-    tarea_palabra = BuscarTareaPorPalabra(pTareas, cantTareas);
-    if(tarea_palabra != NULL){
-        printf("\n------- TAREA ENCONTRADA -------\n");
-        printf("Tarea ID: %d\n", tarea_palabra->TareaID);
-        printf("Descripcion: %s\n", tarea_palabra->Descripcion);
-        printf("Duracion: %d\n", tarea_palabra->Duracion);
-    }
+    // tarea_palabra = BuscarTareaPorPalabra(pTareas, cantTareas);
+    // if(tarea_palabra != NULL){
+    //     printf("\n------- TAREA ENCONTRADA -------\n");
+    //     printf("Tarea ID: %d\n", tarea_palabra->TareaID);
+    //     printf("Descripcion: %s\n", tarea_palabra->Descripcion);
+    //     printf("Duracion: %d\n", tarea_palabra->Duracion);
+    // }
 
-    free(pTareas);
-    free(pTareasHechas);
+    // free(pTareas);
+    // free(pTareasHechas);
 
     return 0;
 }
@@ -80,67 +80,67 @@ void CargarTareas(NODO *lista, int cantidad){
     }
 }
 
-void mostrarTarea(TAREA **tarea, int cantidad){
+void mostrarTarea(NODO *lista, int cantidad){
 
     for(int i=0; i < cantidad; i++){
         printf("------Mostrar Tareas----------\n");
-        printf("----Tarea ID [%d]----\n",(*tarea)->TareaID);
-        printf("Descripcion de la tarea:%s\n",(*tarea)->Descripcion);
-        printf("Duracion de la tarea:%d\n",(*tarea)->Duracion);
-        tarea++;
+        printf("----Tarea ID [%d]----\n",(lista->T.TareaID));
+        printf("Descripcion de la tarea:%s\n",(lista->T.Descripcion));
+        printf("Duracion de la tarea:%d\n",(lista->T.Duracion));
+        lista = lista->Siguiente;
     }
 }
 
-void moverTareas(TAREA **pTareas, TAREA **pTareasHechas, int cantidad){
-    int si_o_no;
+// void moverTareas(TAREA **pTareas, TAREA **pTareasHechas, int cantidad){
+//     int si_o_no;
 
-    printf("\n---------- Tareas Pendientes ----------\n");
-    for (int i = 0; i < cantidad; i++)
-    {
-        if (*(pTareas + i) != NULL)
-        {
-            printf("----------------------------------------\n");
-            printf("Tarea ID: %d\n", pTareas[i]->TareaID);
-            printf("Descripcion: %s\n", pTareas[i]->Descripcion);
-            printf("Duracion: %d\n", pTareas[i]->Duracion);
-            printf("\nMover a realizadas? (1 = SI; 0 = NO)-----\n");
-            scanf("%d", &si_o_no);
+//     printf("\n---------- Tareas Pendientes ----------\n");
+//     for (int i = 0; i < cantidad; i++)
+//     {
+//         if (*(pTareas + i) != NULL)
+//         {
+//             printf("----------------------------------------\n");
+//             printf("Tarea ID: %d\n", pTareas[i]->TareaID);
+//             printf("Descripcion: %s\n", pTareas[i]->Descripcion);
+//             printf("Duracion: %d\n", pTareas[i]->Duracion);
+//             printf("\nMover a realizadas? (1 = SI; 0 = NO)-----\n");
+//             scanf("%d", &si_o_no);
 
-            if (si_o_no == 1)
-            {
-                *(pTareasHechas + i) = *(pTareas + i);
-                *(pTareas + i) = NULL;
-            }else{
-                *(pTareasHechas + i) = NULL;
-            }
-        }
-    }
-}
+//             if (si_o_no == 1)
+//             {
+//                 *(pTareasHechas + i) = *(pTareas + i);
+//                 *(pTareas + i) = NULL;
+//             }else{
+//                 *(pTareasHechas + i) = NULL;
+//             }
+//         }
+//     }
+// }
 
-void mostrarTareasHechasYPendientes(TAREA **pTareasHechas, TAREA **pTareasPendientes, int cantidad){
+// void mostrarTareasHechasYPendientes(TAREA **pTareasHechas, TAREA **pTareasPendientes, int cantidad){
 
 
-    printf("\n---------- TAREAS REALIZADAS ----------\n");
-    for (int i = 0; i < cantidad; i++)
-    {
-        if (pTareasHechas[i] != NULL)
-        {
-            printf("----------------------------------------\n");
-            printf("Tarea ID: %d\n", pTareasHechas[i]->TareaID);
-            printf("Descripcion: %s\n", pTareasHechas[i]->Descripcion);
-            printf("Duracion: %d\n", pTareasHechas[i]->Duracion);
-        }
-    }
+//     printf("\n---------- TAREAS REALIZADAS ----------\n");
+//     for (int i = 0; i < cantidad; i++)
+//     {
+//         if (pTareasHechas[i] != NULL)
+//         {
+//             printf("----------------------------------------\n");
+//             printf("Tarea ID: %d\n", pTareasHechas[i]->TareaID);
+//             printf("Descripcion: %s\n", pTareasHechas[i]->Descripcion);
+//             printf("Duracion: %d\n", pTareasHechas[i]->Duracion);
+//         }
+//     }
 
-    printf("\n---------- TAREAS PENDIENTES ----------\n");
-    for (int i = 0; i < cantidad; i++)
-    {
-        if (pTareasPendientes[i] != NULL)
-        {
-            printf("----------------------------------------\n");
-            printf("Tarea ID: %d\n", pTareasPendientes[i]->TareaID);
-            printf("Descripcion: %s\n", pTareasPendientes[i]->Descripcion);
-            printf("Duracion: %d\n", pTareasPendientes[i]->Duracion);
-        }
-    }
-}
+//     printf("\n---------- TAREAS PENDIENTES ----------\n");
+//     for (int i = 0; i < cantidad; i++)
+//     {
+//         if (pTareasPendientes[i] != NULL)
+//         {
+//             printf("----------------------------------------\n");
+//             printf("Tarea ID: %d\n", pTareasPendientes[i]->TareaID);
+//             printf("Descripcion: %s\n", pTareasPendientes[i]->Descripcion);
+//             printf("Duracion: %d\n", pTareasPendientes[i]->Duracion);
+//         }
+//     }
+// }
